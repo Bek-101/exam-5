5416051c862c286cff9b44050d8eaad50d761424
Bek-101 <dimamikhailov101@gmail.com>
2023-09-27 11:01:08 +0500

    task

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const draggableElement = document.getElementById('draggable');
+const draggableElm = document.getElementById('draggable-one');
 const dropContainer = document.getElementById('drop-container');
-
-draggableElement.addEventListener('dragstart', (e) => {
+draggableElm.addEventListener('dragstart', (e) => {
     e.dataTransfer.setData('text/plain', 'Drag me!');
 });
-
+draggableElement.addEventListener('dragstart', (e) => {
+ e.dataTransfer.setData('text/plain', 'Drag me!');
+});
 dropContainer.addEventListener('dragover', (e) => {
     e.preventDefault();
 });
-
 dropContainer.addEventListener('drop', (e) => {
     e.preventDefault();
     const data = e.dataTransfer.getData('text/plain');
     e.target.textContent = data;
 });
+
